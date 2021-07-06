31e88eaedc91c965a3ef95f4b954eae0ac0063d9
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-06 18:49:57 +0100

    Refactored tests and improved code quality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,29 +1,36 @@
+# This class deals with the oystercard functionality
 class Oystercard
-attr_reader :balance, :in_use, :entry_station
-MAX_BALANCE = 90
-MIN_BALANCE = 1
-    def initialize
-        @balance = 0
-    end
-    def top_up(amount)
-        if @balance + amount > MAX_BALANCE then raise "Balance exceeds maximum amount (#{MAX_BALANCE})" else @balance = @balance + amount end
-    end
-    
-    def touch_in(entry_station)
-        if @balance < MIN_BALANCE then raise "Balance is below minimum amount (#{MIN_BALANCE})" end
-        @entry_station = entry_station
-        end
-    def in_journey?
-        !!entry_station
-        # if @entry_station != nil then true else false end
-    end
-    def touch_out
-        @entry_station = nil
-        deduct(MIN_BALANCE)
-    end
-    
-private 
-    def deduct(amount)
-        @balance = @balance - amount
-    end
+  attr_reader :balance, :in_use, :entry_station
+
+  MAX_BALANCE = 90
+  MIN_BALANCE = 1
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance + amount > MAX_BALANCE ? (raise "Balance exceeds maximum amount (#{MAX_BALANCE})") : (@balance += amount)
+  end
+
+  def touch_in(entry_station)
+    raise "Balance is below minimum amount (#{MIN_BALANCE})" if @balance < MIN_BALANCE
+
+    @entry_station = entry_station
+  end
+
+  def in_journey?
+    !!entry_station
+  end
+
+  def touch_out
+    @entry_station = nil
+    deduct(MIN_BALANCE)
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,50 +1,46 @@
 require 'oystercard'
 
 describe Oystercard do
-    let(:oystercard) { Oystercard.new }
-    let(:station) { double :station}
+  let(:oystercard) { Oystercard.new }
+  let(:station) { double :station }
 
-    it {is_expected.to respond_to(:balance)}
-    it 'given balance 0 when topping up then new balance is top up amount' do
-        oystercard.top_up(10)
-        expect(oystercard.balance).to eq 10
+  it { is_expected.to respond_to(:balance) }
+  it 'given balance 0 when topping up then new balance is top up amount' do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq 10
+  end
+  it 'given balance exceeds 90 when topping up then raise error' do
+    expect { oystercard.top_up(100) }.to raise_error(RuntimeError)
+  end
+  it 'given balance below minimum when touch in then raise error' do
+    expect { oystercard.touch_in(station) }.to raise_error(RuntimeError)
+  end
+
+  context 'given a positive balance and touched in' do
+    before(:each) do
+      oystercard.top_up(10)
+      oystercard.touch_in(station)
     end
-    it 'given balance exceeds 90 when topping up then raise error' do
-        expect{oystercard.top_up(100)}.to raise_error(RuntimeError)
+    it 'then remember entry station' do
+      expect(oystercard.entry_station).to eq station
+    end
+    it 'then in journey' do
+      expect(oystercard.in_journey?).to be true
     end
 
-    context 'given a positive balance' do
-        before(:each) do
-            oystercard.top_up(10)
-            oystercard.touch_in('South Norwood')
-        end
-        it 'given touched out when touching in then in journey' do
-            expect(oystercard.in_journey?).to be true
-        end
-        it 'given touched in when touching out then not in journey' do
-            oystercard.touch_out
-            expect(oystercard.in_journey?).to be false
-        end
-        it 'given entry station not nil when touched in then in journey' do
-            oystercard.touch_in('South Norwood')
-            expect(oystercard.in_journey?).to eq true
-        end
-        it 'given touched out entry station is then nil' do
-            oystercard.touch_out
-            expect(oystercard.in_journey?).to eq false
-        end
-        it 'given touched in when touching out then deduct min fare' do
-            expect {oystercard.touch_out}.to change{oystercard.balance}.by(-Oystercard::MIN_BALANCE)
-        end
-        it 'when touch in then remember entry station' do
-            expect(oystercard.entry_station).to eq 'South Norwood'
-        end
-        it 'when touch out then forgets entry station' do
-            oystercard.touch_out
-            expect(oystercard.entry_station).to be_nil
-        end
+    context 'when touched out' do
+      before(:each) do
+        oystercard.touch_out
+      end
+      it 'then not in journey' do
+        expect(oystercard.in_journey?).to be false
+      end
+      it 'then entry station is nil' do
+        expect(oystercard.entry_station).to be_nil
+      end
+      it 'then deduct min fare' do
+        expect { oystercard.touch_out }.to change { oystercard.balance }.by(-Oystercard::MIN_BALANCE)
+      end
     end
-    it 'given touch in when below minimum balance then raise error' do
-        expect{oystercard.touch_in(oystercard.balance)}.to raise_error(RuntimeError)
-    end
-end 
+  end
+end
