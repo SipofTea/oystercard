530dfc8d0e87504ded6d009f0c62f0b48141a02f
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-06 15:22:30 +0100

    Implemented min balance check and tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,24 @@
 class Oystercard
-attr_reader :balance
+attr_reader :balance, :in_use
 MAX_BALANCE = 90
-
+MIN_BALANCE = 1
     def initialize
         @balance = 0
-       
+        @in_use = false
     end
     def top_up(amount)
         if @balance + amount > MAX_BALANCE then raise "Balance exceeds maximum amount (#{MAX_BALANCE})" else @balance = @balance + amount end
+    end
+    def deduct(amount)
+        @balance = @balance - amount
+    end
+    def touch_in(balance)
+        if balance < MIN_BALANCE then raise "Balance is below minimum amount (#{MIN_BALANCE})" else @in_use = true end
+    end
+    def in_journey?
+        @in_use == true ? true : false
+    end
+    def touch_out
+        @in_use = false
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,36 @@
 require 'oystercard'
 
 describe Oystercard do
+    let(:oystercard) { Oystercard.new }
     it {is_expected.to respond_to(:balance)}
     it 'given balance 0 when topping up then new balance is top up amount' do
-        subject.top_up(10)
-        expect(subject.balance).to eq 10
+        oystercard.top_up(10)
+        expect(oystercard.balance).to eq 10
     end
     it 'given balance exceeds 90 when topping up then raise error' do
-        expect{subject.top_up(100)}.to raise_error(RuntimeError)
+        expect{oystercard.top_up(100)}.to raise_error(RuntimeError)
+    end
+    it 'given balance 0 when deducting then the new balance is negative top up amount' do
+        expect(oystercard.deduct(10)).to eq -10
+    end
+
+    context 'given a positive balance' do
+        before(:each) do
+            oystercard.top_up(10)
+            oystercard.touch_in(oystercard.balance)
+        end
+        it 'given touched out when touching in then in journey' do
+            expect(oystercard.in_use).to be true
+            expect(oystercard.in_journey?).to be true
+        end
+        it 'given touched in when touching out then not in journey' do
+            oystercard.touch_out
+            expect(oystercard.in_use).to be false
+            expect(oystercard.in_journey?).to be false
+        end
+    end
+
+    it 'given touch in when below minimum balance then raise error' do
+        expect{oystercard.touch_in(oystercard.balance)}.to raise_error(RuntimeError)
     end
 end
